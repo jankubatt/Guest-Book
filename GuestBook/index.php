ff0aed93db02455b3b9d93a8c5c6c40561dbd196
jankubatt <jankubat.1@seznam.cz>
2020-11-02 08:28:52 +0100

    fix fa icons

--- GuestBook/index.php
+++ GuestBook/index.php
@@ -25,8 +25,8 @@
         <title>Návštěvní kniha</title>
         <meta charset="utf-8">
         <link rel=icon href="img/book.png">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://kit.fontawesome.com/848da1e5f7.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
